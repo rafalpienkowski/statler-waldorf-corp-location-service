95cee06c6a8be283e662d776154f9860ab582f7d
Rafal Pienkowski <rafal.pienkowski@wp.pl>
2018-06-28 15:31:14 +0200

    Changes in docker_entrypoint.sh

--- src/StatlerWaldorfCorp.LocationService/docker_entrypoint.sh
+++ src/StatlerWaldorfCorp.LocationService/docker_entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /pipeline/source/app/publish
-dotnet ef database update
+
+set -e
+
+until dotnet ef database update; do
+>&2 echo "Postgres Server is starting up"
+sleep 1
+done
+
+>&2 echo "Postgres Server is up - executing command"
+
 dotnet StatlerWaldorfCorp.LocationService.dll
