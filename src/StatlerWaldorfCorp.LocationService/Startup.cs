388a25691f40c885ae8ec311ae85a8630d5c99af
Rafal Pienkowski <rafal.pienkowski@wp.pl>
2018-07-01 12:04:52 +0200

    Auto migrations added.

--- src/StatlerWaldorfCorp.LocationService/Startup.cs
+++ src/StatlerWaldorfCorp.LocationService/Startup.cs
@@ -54,6 +54,22 @@ namespace StatlerWaldorfCorp.LocationService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            try
+            {
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+                {
+                    var context = serviceScope.ServiceProvider.GetService<LocationDbContext>();
+                    if (!context.Database.EnsureCreated())
+                    {
+                        context.Database .Migrate();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to migrate or seed database");
+            }
+
             app.UseMvc();
         }
     }
--- src/StatlerWaldorfCorp.LocationService/docker_entrypoint.sh
+++ src/StatlerWaldorfCorp.LocationService/docker_entrypoint.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 cd /pipeline/source/app/publish
-
-set -e
-
-until dotnet ef database update; do
->&2 echo "Postgres Server is starting up"
-sleep 1
-done
-
->&2 echo "Postgres Server is up - executing command"
-
 dotnet StatlerWaldorfCorp.LocationService.dll
